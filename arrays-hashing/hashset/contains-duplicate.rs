67fb59c3d74ab3ac854dd546a8137a799e98de8c
derdelean <dennis.erdelean@proton.me>
2026-04-19 16:24:27 +0200

    contains duplicate, rust, impl, struct

--- arrays-hashing/hashset/contains-duplicate.rs
+++ arrays-hashing/hashset/contains-duplicate.rs
@@ -1,13 +1,27 @@
-use std::collection::HashSet;
+/*
+Task: Given an integer array nums, return true if any value appears more than once in the array,
+otherwise return false
 
-fn contains_duplicate(nums: Vec<i32>) -> bool {
-    let mut visited = HashSet::new();
+Time: O(n) - Each operation takes O(1) checking/scanning array once so O(n)
+Space: O(n) - HashSet creation -> Size can be up to the size of input so O(n)
 
-    for n in nums {
-        if visited.contains(&n) {
-            return true;
+Check Array againt HashSet with visited elements
+*/
+
+use std::collections::HashSet;
+
+struct Solution;
+
+impl Solution {
+    fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut visited = HashSet::new();
+
+        for n in nums {
+            if visited.contains(&n) {
+                return true;
+            }
+            visited.insert(n);
         }
-        visited.insert(n);
+        false
     }
-    false
 }
